8f872b5804bcac8b1108dc67dd9ad1e88918e5dd
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-09-14 02:11:15 +0100

    Back end for email management app

--- email_sender.js
+++ email_sender.js
@@ -1,14 +1,39 @@
- function sendEmail (){
+const bcrypt = require('bcrypt')
+import { SMTPClient } from 'emailjs';
 
-         Email.send({
-             Host : "s1.maildns.net",
-             Username : "username",
-             Password : "password",
-             To : 'them@website.com',
-             From : "you@isp.com",
-             Subject : "This is the subject",
-             Body : "And this is the body"
-         }).then(
-             message => alert(message)
-         );
+function sendEmail (){
+
+    const client = new SMTPClient({
+        user: 'user',
+        password: 'password',
+        host: 'smtp.your-email.com',
+        ssl: true,
+    });
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+
+    client.send(
+        {
+            from: 'edogbanya02@gmail.com',
+            to: email,
+            cc: 'else <else@your-email.com>',
+            company: name,
+            subject: subject,
+            text: message,
+        },
+        (err, message) => {
+            console.log(err || message);
+        }
+    );
  }
+
+  function encryptPassword (){
+    const password = document.getElementById("password").value;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    console.log(hash);
+  }
